c1facb16cb541254406412c240df83c5b753bf5e
KevinJosueChavezBarragan <65294584+KevinJosueChavezBarragan@users.noreply.github.com>
2022-10-12 09:23:27 -0600

    Add files via upload

--- EVA2_3_LISTA_DOBLE.java
+++ EVA2_3_LISTA_DOBLE.java
@@ -1,21 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package eva2_3_lista_doble;
 
-/**
- *
- * @author moviles
- */
 public class EVA2_3_LISTA_DOBLE {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        ListaDoble listaDoble = new ListaDoble();
+        
+        listaDoble.agregar(10);
+        listaDoble.agregar(20);
+        listaDoble.agregar(30);
+        listaDoble.agregar(40);
+        listaDoble.agregar(50);
+        listaDoble.agregar(60);
+        listaDoble.imprimir();
     }
     
 }
