6bd1c29860b0f61fe88126fd49318dbff201c78e
KevinJosueChavezBarragan <65294584+KevinJosueChavezBarragan@users.noreply.github.com>
2022-10-13 09:49:39 -0600

    Add files via upload

--- EVA2_3_LISTA_DOBLE.java
+++ EVA2_3_LISTA_DOBLE.java
@@ -1,5 +1,8 @@
 package eva2_3_lista_doble;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class EVA2_3_LISTA_DOBLE {
 
     public static void main(String[] args) {
@@ -11,7 +14,23 @@ public class EVA2_3_LISTA_DOBLE {
         listaDoble.agregar(40);
         listaDoble.agregar(50);
         listaDoble.agregar(60);
+        listaDoble.imprimir();   
+        
+        try {
+            listaDoble.agregarEn(70, 5);
+        } catch (Exception ex) {
+            Logger.getLogger(EVA2_3_LISTA_DOBLE.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
         listaDoble.imprimir();
+        
+        try {
+            listaDoble.borrarEn(5);
+        } catch (Exception ex) {
+            Logger.getLogger(EVA2_3_LISTA_DOBLE.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        listaDoble.imprimir();    
     }
     
 }
--- ListaDoble.java
+++ ListaDoble.java
@@ -56,10 +56,80 @@ public class ListaDoble {
         cont++;
     }
     
+    public void agregarEn(int valor, int pos) throws Exception {
+        //QUE DEBEMOS VALIDAR
+
+        int cantNodos = tamaLista();
+        if (pos < 0) {//posiciones negativas
+            throw new Exception("No puede insertar un nodo en una posición negativa");
+        } else if (pos >= cantNodos) {//INSERTAR EN POSICIONES NO VALIDAS
+            throw new Exception(pos + " no es una posicion válida");
+        } else {
+            Nodo nuevoNodo = new Nodo(valor);
+            if (pos == 0) {//insertar al inicio de la lista
+                nuevoNodo.setSiguiente(inicio);
+                inicio.setPrevio(nuevoNodo);
+                inicio = nuevoNodo;
+            } else {
+                Nodo temp = inicio;
+                int cont = 0;
+                //¿COMO MUEVO A TEMP?
+                while (cont < pos) {
+                    temp = temp.getSiguiente();
+                    cont++;
+                }
+                nuevoNodo.setSiguiente(temp);
+                nuevoNodo.setPrevio(temp.getPrevio());
+                temp.getPrevio().setSiguiente(nuevoNodo);
+                temp.setPrevio(nuevoNodo);
+
+            }
+            this.cont++;
+        }
+    }
+    
     public void vaciarLista() {
         inicio = null;
         fin = null;
         cont = 0;
+    }
+    
+    public void borrarEn(int pos) throws Exception {
+
+        int cantNodos = tamaLista();
+        if (pos < 0) {//posiciones negativas
+            throw new Exception("No puede borrar un nodo en una posición negativa");
+        } else if (pos >= cantNodos) {//INSERTAR EN POSICIONES NO VALIDAS
+            throw new Exception(pos + " no es una poscion válida");
+        } else {
+            if (cantNodos == 1) {//borrar el primer nodo
+                vaciarLista();
+            } else {
+                //borrar primer nodo (Listo)
+                //borrar el intermedio
+                //borrar el final
+                if (pos == 0) {
+                    inicio = inicio.getSiguiente();
+                } else {
+                    Nodo temp = inicio;
+                    int cont = 0;
+                    //¿COMO MUEVO A TEMP?
+                    while (cont < pos) {
+                        temp = temp.getSiguiente();
+                        cont++;
+                    }
+                    Nodo objSig = temp.getSiguiente();
+                    Nodo objPrev = temp.getPrevio();
+                    temp.setSiguiente(objSig.getSiguiente());
+                    temp.setPrevio(objPrev.getPrevio());
+
+                    if (pos == (cantNodos - 1)) {//reconectar fin
+                        fin = temp;
+                    }
+                }
+                this.cont--;
+            }
+        }
     }
     
     public int obtenerValorEn(int pos) throws Exception {
