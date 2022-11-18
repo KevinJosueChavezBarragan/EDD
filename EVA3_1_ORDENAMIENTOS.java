4e95acc3a146e2696271015911b2fb83ef07ca7a
KevinCB <65294584+KevinJosueChavezBarragan@users.noreply.github.com>
2022-11-18 09:51:26 -0600

    Add files via upload

--- EVA3_1_ORDENAMIENTOS.java
+++ EVA3_1_ORDENAMIENTOS.java
@@ -10,19 +10,28 @@ public class EVA3_1_ORDENAMIENTOS {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int[] arregloDatos = new int[100];
+        int[] arregloDatos = new int[15];
         int[] arregloSel = new int[arregloDatos.length];
+        int[] arregloIns = new int[arregloDatos.length];
         System.out.println("ARREGLO ORIGINAL:");
         llenar(arregloDatos);
-        //imprimir(arregloDatos);
+        imprimir(arregloDatos);
         System.out.println("SELECTION SORT:");
         copiar(arregloDatos, arregloSel);
-        //imprimir(arregloSel);
+        imprimir(arregloSel);
         double ini = System.nanoTime();
         selectionSort(arregloSel);
         double fin = System.nanoTime();
-        //imprimir(arregloSel);
+        imprimir(arregloSel);
         System.out.println("Selection sort = " + (fin - ini));
+        System.out.println("INSERTION SORT:");
+        copiar(arregloDatos, arregloIns);
+        imprimir(arregloIns);
+        ini = System.nanoTime();
+        insertionSort(arregloIns);
+        fin = System.nanoTime();
+        imprimir(arregloIns);
+        System.out.println("Insertion sort = " + (fin - ini));
     }
     
     //llenar arreglo
@@ -65,6 +74,26 @@ public class EVA3_1_ORDENAMIENTOS {
                 arreglo[i] = arreglo[min];
                 arreglo[min] = temp;
             }
+        }
+    }
+    
+    //INSERTION SORT
+    public static void insertionSort(int[] arreglo){
+        for (int i = 1; i < arreglo.length; i++) {
+            int temp = arreglo[i];
+            int insP = i;
+            //BUSCAMOS EN QUE PUNTO DEBE DE QUEDAR TEMP
+            //ESA POSICION ES ALMACENADA EN INSP
+            for (int prev = i-1; prev >= 0; prev--) {
+                if(arreglo[prev] > temp){
+                    //swap(intercambio parcial)
+                    arreglo[insP] = arreglo[prev];
+                    insP--;
+                }else{
+                    break;
+                }
+            }
+            arreglo[insP] = temp;
         }
     }
 }
